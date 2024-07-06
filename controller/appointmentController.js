074b92ec57841a604e97698864816be64a37b0ea
AnandsinghKKR <jitendrapalsingh465@gmail.com>
2024-07-06 11:24:25 +0530

    deleting appointment functionality added

--- controller/appointmentController.js
+++ controller/appointmentController.js
@@ -105,6 +105,18 @@ export const updateAppointmentStatus=catchAsyncErrors(async(req,res,next)=>{
         message:"Appointment Status Updated",
         appointment,
     })
+});
 
-
+export const deleteAppointment=catchAsyncErrors(async(req,res,next)=>{
+    const {id}=req.params;
+    let appointment=await Appointment.findById(id);
+    if(!appointment){
+        return next(new ErrorHandler("Appointment Not Found",404))
+    }
+    // for deleting the appointment
+    await appointment.deleteOne();
+    res.status(200).json({
+        success:true,
+        message:"Appointment Deleted"
+    })
 })
--- router/appointmentRouter.js
+++ router/appointmentRouter.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getAllAppointments, postAppointment, updateAppointmentStatus } from "../controller/appointmentController.js";
+import { deleteAppointment, getAllAppointments, postAppointment, updateAppointmentStatus } from "../controller/appointmentController.js";
 import { isAdminAuthenticated,isPatientAuthenticated } from "../middlewares/auth.js";
 
 const router=express.Router();
@@ -7,6 +7,6 @@ const router=express.Router();
 router.post("/post",isPatientAuthenticated,postAppointment); // jo appointment post karega woh patient hona chahiye
 router.get("/getall",isAdminAuthenticated,getAllAppointments);
 router.put("/update/:id",isAdminAuthenticated,updateAppointmentStatus); // status ko update kar rhe hai
-
+router.delete("/delete/:id",isAdminAuthenticated,deleteAppointment); // deleteing appointment
 
 export default router;
